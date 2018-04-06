2026fca9a43cb4f6a580ad1d3479cf6df2fe7460
Nina Luo <30792189+Ni4991@users.noreply.github.com>
2018-04-06 00:22:01 -0700

    Create lanchat.py

---
+++ lanchat.py
@@ -0,0 +1,80 @@
+import sys
+import socket
+import select
+from sys import argv
+from scapy.all import *
+import builtins
+
+def scan():
+    print("===========Lan Scanner===========")
+    
+    while True:
+        try:
+            interface = raw_input("Enter network interface: ")
+            ip = raw_input("Enter IP: ")
+            
+            print("Scanning")
+
+            conf.verb = 0
+            ans, unans = srp(Ether(dst = "ff:ff:ff:ff:ff:ff")/ARP(pdst = ip), timeout = 2, iface = interface, inter = 0.1)
+
+            print("MAC Address - IP Address")
+            for send, recv in ans:
+                print(recv.sprintf(r"%Ether.src% - %ARP.psrc%"))
+            print("Scan ended")
+
+        except KeyboardInterrupt:
+            print("Scanner shutdown by user")
+            break
+
+    print("Scan complete")
+
+def send():
+    print("==========UDP Chat Sender==========")
+    
+    host = raw_input("Enter IP: ")
+    print("Enter space to set default port 1027" )
+    user_port = raw_input("Connect to Port Number: ")
+    if user_port == "":
+        port = int("1027", 16)
+    else:
+        port = int(user_port, 16)
+
+    send_address = (host, port)
+    
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1) 
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.setblocking(False)
+    sock.bind(('', port))
+
+    print("Press 'ctrl + c' to exit")
+    print("Connecting to port", hex(port))
+    print("Type your message")
+
+    while True:
+        try:
+            message, address = sock.recvfrom(5000)
+            message = message.strip("\n")
+            if message:
+                print(address, "->", message)
+        except:
+            pass
+        
+        message = getLine();
+        if message != False:
+            sock.sendto(message, send_Address)
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: python lanchat.py [scan] [send]")
+        return
+
+    if sys.argv[1] == "scan":
+        scan()
+    
+    if sys.argv[1] == "send":
+        send()
+
+if __name__ == '__main__':
+    main()
